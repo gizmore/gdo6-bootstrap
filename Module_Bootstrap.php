f57987d00954a1d9d51c56fdf8fb93261bed054b
gizmore <gizmore@wechall.net>
2019-11-24 05:26:19 +0100

    6.10-Bootstrap_Theme

--- Module_Bootstrap.php
+++ Module_Bootstrap.php
@@ -8,11 +8,13 @@ final class Module_Bootstrap extends GDO_Module
 {
 	public $module_priority = 10;
 	
+	public function getDependencies() { return ['JQuery']; }
+	
 	public function onIncludeScripts()
 	{
 		$min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
 
-		$this->addBowerJavascript("bootstrap/dist/js/bootstrap$min.js");
+		$this->addBowerJavascript("bootstrap/dist/js/bootstrap.bundle$min.js");
 		$this->addBowerCSS("bootstrap/dist/css/bootstrap$min.css");
 
 		$this->addCSS("css/gdo-bootstrap.css");
