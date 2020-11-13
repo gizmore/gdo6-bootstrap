7da261dff77c2519789cccc8d34b6e062f42e81c
gizmore <gizmore@wechall.net>
2020-11-13 01:43:02 +0100

    6.10-r9371_only_1_theme_per_module

--- Module_Bootstrap.php
+++ Module_Bootstrap.php
@@ -3,21 +3,47 @@ namespace GDO\Bootstrap;
 
 use GDO\Core\GDO_Module;
 use GDO\Core\Module_Core;
+use GDO\DB\GDT_Checkbox;
 
+/**
+ * Bootstrap4 includes.
+ * Can be made non include if only a theme needs it.
+ * Has JQuery and Moment dependency
+ * 
+ * @author gizmore
+ * @version 6.10
+ * @since 6.06
+ */
 final class Module_Bootstrap extends GDO_Module
 {
 	public $module_priority = 15;
 	
 	public function getDependencies() { return ['JQuery', 'Moment']; }
 	
+	public function getConfig()
+	{
+	    return [
+	        GDT_Checkbox::make('include_bootstrap')->initial('1'),
+	    ];
+	}
+	public function cfgIncludeBootstrap() { return $this->getConfigValue('include_bootstrap'); }
+	
 	public function onIncludeScripts()
 	{
-		$min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
-
-		$this->addBowerJavascript("bootstrap/dist/js/bootstrap.bundle$min.js");
-		$this->addBowerCSS("bootstrap/dist/css/bootstrap$min.css");
-
-		$this->addCSS("css/gdo-bootstrap.css");
+	    if ($this->cfgIncludeBootstrap())
+	    {
+	        $this->onIncludeBootstrap4();
+	    }
+	}
+	
+	public function onIncludeBootstrap4()
+	{
+	    $min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
+	    
+	    $this->addBowerJavascript("bootstrap/dist/js/bootstrap.bundle$min.js");
+	    $this->addBowerCSS("bootstrap/dist/css/bootstrap$min.css");
+	    
+	    $this->addCSS("css/gdo-bootstrap.css");
 	}
 	
 }
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # gdo6-bootstrap
-Bootstrap includes for gdo6.
+Bootstrap4 includes for gdo6. Bootstrap3 includes are available at [gizmore/gdo6-bootstrap3](https://github.com/gizmore/gdo6-bootstrap3).
+
+## Dependencies
+This module depends on
+- [gizmore/gdo6-jquery](https://github.com/gizmore/gdo6-jquery)
+- [gizmore/gdo6-moment](https://github.com/gizmore/gdo6-moment)
+
+## Installation
+Read the gdo6 install guide on [gizmore/gdo6](https://github.com/gizmore/gdo6 "GDO6 core").
+If you want to install the module without the wizard you can use these commands.
+
+    cd gdo6/GDO/
+    git clone --recursive https://github.com/gizmore/gdo6-bootstrap Bootstrap
+    cd Bootstrap
+    yarn install
+    cd gdo6/
+    php gdo.php install Bootstrap
+
+## Configuration
+You can disable the asset inclusion in case you use multiple themes with different dependencies.
+[gizmore/gdo6-bootstrap-theme](https://github.com/gizmore/gdo6-bootstrap-theme "GDO6 Bootstrap Theme") makes use of this.
+
+### Enjoy!
+gizmore
